791a3bdf670a9f71d771df3cba086aa90f6cf382
Yana Wahyuna <yana.wahyuna@epson.co.id>
2024-01-04 15:37:01 +0700

    UI
    MainActivity implementation

--- app/src/main/java/com/fwhyn/fragmentanddialog/main/MainActivity.kt
+++ app/src/main/java/com/fwhyn/fragmentanddialog/main/MainActivity.kt
@@ -2,14 +2,21 @@ package com.fwhyn.fragmentanddialog.main
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.absolutePadding
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.compose.ui.unit.dp
 import com.fwhyn.fragmentanddialog.common.BaseActivity
 import com.fwhyn.fragmentanddialog.theme.FragmentAndDialogTheme
 
@@ -17,18 +24,33 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val splashScreen = installSplashScreen()
-
         setContent {
-            FragmentAndDialogTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+            Content()
+        }
+    }
+}
+
+@Composable
+fun Content() {
+    FragmentAndDialogTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Column(
+                modifier = Modifier
+                    .absolutePadding(left = 0.dp, top = 16.dp, right = 0.dp, bottom = 16.dp)
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Greeting("Android")
+                Spacer(modifier = Modifier.height(16.dp))
+                ElevatedButtonExample {
+
                 }
             }
+
         }
     }
 }
@@ -41,10 +63,15 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    FragmentAndDialogTheme {
-        Greeting("Android")
+fun ElevatedButtonExample(onClick: () -> Unit) {
+    ElevatedButton(onClick = { onClick() }) {
+        Text("Show Dialog")
     }
+}
+
+@Preview(showBackground = true, widthDp = 412, heightDp = 915)
+@Composable
+fun ContentPreview() {
+    Content()
 }
